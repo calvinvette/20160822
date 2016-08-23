454113fd86642a7b062ca0e1fee194b56861bc57
calvinvette <calvin.vette@nextgeneducation.com>
2016-08-23 12:41:03 -0400

    Lunch Tuesday

---
+++ app/src/main/java/com/nextgened/dnd/diceroller/MainActivity.java
@@ -0,0 +1,50 @@
+package com.nextgened.dnd.diceroller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity {
+      @BindView(R.id.tfUserName) EditText tfUserName;
+    @BindView(R.id.tfEmail) EditText tfEmail;
+    @BindView(R.id.tfBirthDate) EditText tfBirthDate;
+    @BindView(R.id.tvHello) TextView tvHello;
+
+    // Attach in activity_main.xml's by setting btnRegister's onClick method to the method name
+    @OnClick(R.id.btnRegister)
+    public void registerUser(View button) {
+        //EditText tfUserName = (EditText) findViewById(R.id.tfUserName);
+        String userName = tfUserName.getText().toString();
+
+//        String birthDate = ((EditText) findViewById(R.id.tfBirthDate)).getText().toString();
+        String birthDate = tfBirthDate.getText().toString();
+
+//        String email = ((EditText) findViewById(R.id.tfEmail)).getText().toString();
+        String email = tfEmail.getText().toString();
+
+        Log.i(MainActivity.class.getName(), "User Name: " + userName);
+        Log.i(MainActivity.class.getName(), "BirthDate: " + birthDate);
+        Log.i(MainActivity.class.getName(), "Email: " + email);
+
+        tvHello.setText("Hello, " + userName);
+        String registered_user = getString(R.string.registered_user, userName);
+        Toast.makeText(this, registered_user, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+}
